396163de71f4ab211429d2015ca523cac6467fe1
Alex Thomas <apt9jf_uva@bitbucket.org>
2013-04-04 00:38:13 -0400

    Added most components for Google Maps

---
+++ UVaTour/README.txt
@@ -0,0 +1,9 @@
+README
+
+Project Manager:	Ram Ramkumar
+Software Architect: 	Alex Thomas
+Gui Designer:		Brent Baumgartner
+QA Manager:		Jack Dingilian
+
+
+See wiki for more info
---
+++ UVaTour/src/com/example/uvatour/GoogleMaps.java
@@ -0,0 +1,13 @@
+package com.example.uvatour;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GoogleMaps extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.google_maps);
+	}
+}
