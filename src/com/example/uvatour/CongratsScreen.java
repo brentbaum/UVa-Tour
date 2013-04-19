51caa7e8311f862677a6b595c6976e97a0cdd2c0
JackDing <jtd5qe@virginia.edu>
2013-04-19 14:48:18 -0400

    Created congratscreen.xml and CongratsScreen.java.  Implemented videoview on congrats screen

---
+++ src/com/example/uvatour/CongratsScreen.java
@@ -0,0 +1,44 @@
+package com.example.uvatour;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.VideoView;
+
+public class CongratsScreen extends Activity {
+
+	// fields
+
+		@Override
+		
+		
+		public void onCreate(Bundle savedInstanceState) {
+		       super.onCreate(savedInstanceState);
+		       setContentView(R.layout.congratscreen);
+		       
+		       //Sets up viedeoview for congratscreen
+		       VideoView videoView = (VideoView)findViewById(R.id.videoView1);
+		       videoView.setKeepScreenOn(true);
+		       videoView.setVideoPath("sdcard/wahoowa.mp4");
+		       videoView.start();
+		       videoView.requestFocus();   
+		}
+
+		@Override
+		public boolean onCreateOptionsMenu(Menu menu) {
+			// Inflate the menu; this adds items to the action bar if it is present.
+			getMenuInflater().inflate(R.menu.tourscreen, menu);
+			return true;
+		}
+
+		// called when restartButton is clicked
+		public void toMapScreen(View view) {
+			Context context = this;
+			Intent intent = new Intent(context, MapScreen.class);
+			startActivity(intent);
+		}
+		
+}
