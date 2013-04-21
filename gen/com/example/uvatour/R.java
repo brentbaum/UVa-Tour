8f977d9789a0ed4942ab1ac18c75b7418c1657b2
Brent Baumgartner <tennisplayer2341@gmail.com>
2013-04-21 16:46:16 -0400

    Added properly to index

--- gen/com/example/uvatour/R.java
+++ gen/com/example/uvatour/R.java
@@ -140,11 +140,10 @@ containing a value of this type.
     }
     public static final class drawable {
         public static final int card_background=0x7f020000;
-        public static final int color_bar_blue=0x7f020001;
-        public static final int color_bar_light_blue=0x7f020002;
-        public static final int color_bar_orange=0x7f020003;
-        public static final int color_bar_red=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
+        public static final int color_bar_light_blue=0x7f020001;
+        public static final int color_bar_orange=0x7f020002;
+        public static final int color_bar_red=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
     }
     public static final class id {
         public static final int button1=0x7f050006;
